3fe32889f3fc5d15ba958c68fe1a673d3d76675c
demidraksin <greatraksin@icloud.com>
2022-01-08 18:50:04 +0300

    Adding contacts page view 🚗

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def index():
     return render_template('index.html', context=context)  # при переходе на главную страницу, показать шаблон index.html
 
 
+@app.route('/contacts')
+def contacts():
+    context = {'title': 'Contacts page'}  # набор элементов, передаваемый в шаблон сайта
+    return render_template('.html', context=context)
+
 if __name__ == '__main__':
     app.run()
