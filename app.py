c0233a66c0f98bad905eebcd269e5f64d1d6070f
demidraksin <greatraksin@icloud.com>
2022-01-08 18:45:04 +0300

    Rendering pages 🧠

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)  # непосредственно сайт
@@ -6,7 +7,8 @@ app = Flask(__name__)  # непосредственно сайт
 
 @app.route('/')  # что будет, если зайти на главную страницу сайта  vk.com/ <-
 def index():
-    return 'Hello World!'
+    context = {'title': 'My first site'}  # набор элементов, передаваемый в шаблон сайта
+    return render_template('index.html', context=context)  # при переходе на главную страницу, показать шаблон index.html
 
 
 if __name__ == '__main__':
